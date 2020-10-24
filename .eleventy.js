35826ad22a85866292a26463d7f30976c52790d7
Kevin Pei <kpei98@gmail.com>
2020-10-24 02:33:42 -0400

    Add domain verification txt

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/img");
   eleventyConfig.addPassthroughCopy("src/static");
+  eleventyConfig.addPassthroughCopy("src/ostrio-domain.txt");
   return {
     dir: {
         input: "src",
