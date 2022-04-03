bd4a32ee56f566220bc068cc699d398f155308e8
Kevin Pei <kpei98@gmail.com>
2022-04-02 18:25:37 -0700

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body, html {
+    margin: 0;
+    padding: 0;
+    font-family: Muli, sans-serif;
+  }
+  h1, h2, h3, h4, h5, h6 {
+      font-family: 'Noto Serif Display', serif;
+  }
+`;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Serif+Display:wght@300;400;700&display=block"
+          rel="stylesheet"
+        />
+        <link rel="stylesheet" href="https://use.typekit.net/dch4hsj.css" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
