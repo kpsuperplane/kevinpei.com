fc5dd3836de4bf24cedd6aaf62498f1188e8a7b7
Kevin Pei <kpei98@gmail.com>
2022-04-08 02:01:39 -0700

    Set vertical align

--- pages/index/Cover.tsx
+++ pages/index/Cover.tsx
@@ -51,7 +51,7 @@ const AnimatedTitle = styled(motion.span)`
   display: inline-block;
   padding-right: 0.5rem;
   line-height: 1.2em;
-  vertical-align: baseline;
+  vertical-align: text-top;
   opacity: 0;
 `;
 
